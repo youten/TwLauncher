306703382cc092913fc52e391ceaeea17efda4d2
youten <youtenredo@gmail.com>
2018-05-29 15:08:56 +0900

    add Launch home category apk

---
+++ app/src/main/java/youten/redo/launchhome/MainActivity.java
@@ -0,0 +1,20 @@
+package youten.redo.launchhome;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent (Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+}
